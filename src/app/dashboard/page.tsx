c0b9112016b1f722fcfb06729a439f46deee80df
MAChapurin <machapurin@icloud.com>
2025-12-08 00:33:17 +0300

    remove api directory

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { sessionService } from "@/entities/user/server";
 import { LogOutButton } from "@/features/auth/ui/logout-button";
 
 export default async function DashboardPage() {
-  const { session } = await sessionService.verifySession();
+  await sessionService.verifySession();
   return (
     <div className="p-8 h-screen">
       <h1>Добро пожаловать на Dashboard!</h1>
